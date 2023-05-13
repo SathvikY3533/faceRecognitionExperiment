c8657197160ed4d58d9e35cf9f65d42eed40faad
SathvikY3533 <sathvik.y007@gmail.com>
2023-05-13 12:36:14 -0500

    Camera setup done

---
+++ script.js
@@ -0,0 +1,26 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time*1000));
+}
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
